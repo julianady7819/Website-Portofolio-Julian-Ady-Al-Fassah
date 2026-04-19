28c489f95be539414371d5c31086d1d9876046a8
Eryza22 <eryza.satria.2205356@students.um.ac.id>
2026-04-19 21:29:30 +0700

    Update semua

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Web-Portofolio-Bima/',
+  base: "/Website-Portofolio-Julian-Ady-Al-Fassah/",
   plugins: [
     react(),        
     tailwindcss(),
